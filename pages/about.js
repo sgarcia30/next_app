53cdf37eea84b823716a92c47e3de06766506c69
Sarah Ann Garcia <sarahanngarcia@Sarahs-MacBook-Pro.local>
2018-11-19 20:06:35 -0500

    new app with Next.js

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Header from '../components/Header'
+import Layout from '../components/MyLayout.js'
+
+export default () => (
+  <div>
+    <Layout>
+      <p>This is the about page</p>
+    </Layout>
+  </div>
+)
